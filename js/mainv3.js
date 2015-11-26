1d245e129a12e9bb0afdee6cb0bcea7dcf627f75
Greta Huang <gretahuang@berkeley.edu>
2015-11-26 00:49:28 -0500

    fix resume javascript

--- js/mainv3.js
+++ js/mainv3.js
@@ -129,25 +129,13 @@ function populateSection( title ) {
     sectionContainer.append(
       '<div class="row">' +
         '<div class="col-sm-12">' +
-          '<div class="project-name"><i class="fa fa-user"></i><br />Awards</div>' +
-          '<div class="project-description resume"></div>' +
         '</div>' +
       '</div>' +
       '<div class="row">' +
         '<div class="col-sm-12">' +
-          '<div class="project-name"><i class="fa fa-trophy"></i><br />Activities</div>' +
-          '<div class="project-description resume"></div>' +
         '</div>' +
       '</div>'
     );
-
-    $.each( contentInfo[title].awards, function(i, val) {
-      sectionContainer.find($(".project-description")).first().append('<div class="resume-line">' + this + '</div>');
-    });
-
-    $.each( contentInfo[title].activities, function(i, val) {
-      sectionContainer.find($(".project-description")).last().append('<div class="resume-line">' + this + '</div>');
-    });
   }
 }
 
@@ -172,4 +160,4 @@ function populateSection( title ) {
   //   // Be sure your document contains an element with the ID 'pdf-row' 
 
   // }
-}
+// }
