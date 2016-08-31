bfd188927c7ce29b09c7c7fe68006fafc2a663ef
Greta Huang <gretahuang@berkeley.edu>
2016-08-30 22:49:31 -0700

    Updates to projects

--- js/content-info.js
+++ js/content-info.js
@@ -2,19 +2,27 @@ var contentInfo = {
     "Coding Projects": {
         "projects": [
             {
+                "title": "UC Berkeley PTPS Website",
+                "image": "ptps_white.png",
+                "slogan": "(Active Webmaster)",
+                "description": "Peoples' Test Preparation Services is a non-profit, student-run organization at the University of California, Berkeley dedicated to providing free standardized test taking skills to under-privileged urban communities.",
+                "link": "https://www.ocf.berkeley.edu/~ptps/",
+                "color": "#009900"
+            },
+            {
                 "title": "LoCality",
                 "image": "locality.png",
                 "slogan": "Rate your landlord, anonymously.",
                 "description": "LoCality allows UC Berkeley students to rate their landlords anonymously (in progress). Students can sponsor their landlords, if they feel that their landlord deserving. A sponsorship gives the landlord access to advertisement on the UC Berkeley specific leasing and subletting portion of the website.  Skills used include HTML, CSS, JavaScript, SQLAlchemy, Flask.",
                 "link": "https://github.com/gretahuang/calhacks2015",
-                "color": "#ffa800"
+                "color": "#e6b800"
             },
             {
                 "title": "Student Progress Tracker",
                 "image": "progresstracker.png",
                 "slogan": "Track your progress.",
                 "description": "The Student Progress Tracker was created for students at Commack High School, to keep track of assignments and hours completed for classes occurring outside of school hours. This app implemented an automated emailing feature, which allowed students and teachers to keep up to date on class requirements. The calendar interface is customizable to teacher- approved goals for the week, allowing students and teachers to more easily keep pace with their advancement.",
-                "link": "https://www.commack.k12.ny.us",
+                "link": "pdf/dossier.pdf",
                 "color": "#007fff"
             },
         ]
@@ -23,7 +31,7 @@ var contentInfo = {
         "projects": [
             {
                 "title": "Nanomedicine",
-                "image": "2013-board.png",
+                "image": "2012-board.png",
                 "slogan": "Investigation of BHBM-Grafted Gold Nanoparticles as Drug Delivery Agents for Treatment of Cryptococcosis",
                 "description": "In this study, gold nanoparticles were fabricated, and then functionalized with a novel drug called BHBM. This drug had implications in the treatment of meningeal and pulmonary Cryptococcosis, a fungal disease. These gold nanoparticles were evaluated for efficacy as a drug delivery system for treatment of meningeal and pulmonary Cryptococcosis. This project took 1st place at the New York Science and Engineering Fair. This research was a part of the Garcia MRSEC program, under the mentorship of Dr. Miriam Rafailovich and Dr. Maurizio Del Poeta at Stony Brook University. ",
                 "link": "pdf/Abstract2013.pdf",
@@ -31,7 +39,7 @@ var contentInfo = {
             },
             {
                 "title": "Nanomedicine",
-                "image": "2012-board.png",
+                "image": "cancer-cell.png",
                 "slogan": "Toward Breast Cancer Therapy and Early Tumor Detection: Receptor Mediated Cellular Uptake of Platinum Folate Nanoparticles and Their Influence on Cellular Mechanics",
                 "description": "Folate coatings on nanoparticles have implications in targeted treatment of cancer cells, whilst platinum cancer therapies already exist and are in use. This study aimed to combine these two findings, created a methodology for synthesis of a folate coated platinum nanoparticle. The study then investigated the novel nanoparticle's uptake in breast cancer cells, and its effectiveness in selective elimination of cancerous cells. The study additionally explored an Atomic Force Microscopy method for cancer detection at the single cell level on basis of differences in rigidity of the cellular membranes of cancerous and non-cancerous cells. This project took 3rd place at the Long Island Science and Engineering Fair. This research was a part of the Garcia MRSEC program, under the mentorship of Dr. Miriam Rafailovich and at Stony Brook University.",
                 "link": "pdf/Abstract2012.pdf",
--- js/mainv3.js
+++ js/mainv3.js
@@ -125,50 +125,27 @@ function populateSection( title ) {
   }
 
   //for resume
-  else {
-    sectionContainer.append(
-      '<div class="row">' +
-        '<div class="col-sm-12">' +
-          '<div class="project-name"><i class="fa fa-user"></i><br />Awards</div>' +
-          '<div class="project-description resume"></div>' +
-        '</div>' +
-      '</div>' +
-      '<div class="row">' +
-        '<div class="col-sm-12">' +
-          '<div class="project-name"><i class="fa fa-trophy"></i><br />Activities</div>' +
-          '<div class="project-description resume"></div>' +
-        '</div>' +
-      '</div>'
-    );
-    $.each( contentInfo[title].awards, function(i, val) {
-      sectionContainer.find($(".project-description")).first().append('<div class="resume-line">' + this + '</div>');
-    });
-
-    $.each( contentInfo[title].activities, function(i, val) {
-      sectionContainer.find($(".project-description")).last().append('<div class="resume-line">' + this + '</div>');
-    });
-  }
-}
-
-  //for resume
   // else {
   //   sectionContainer.append(
-  //     '<div class="row" id="pdf-row">' +
-  //     '<embed src="http://gretahuang.me/resume.pdf" width="100%" height="600" type="application/pdf">' +
+  //     '<div class="row">' +
+  //       '<div class="col-sm-12">' +
+  //         '<div class="project-name"><i class="fa fa-user"></i><br />Awards</div>' +
+  //         '<div class="project-description resume"></div>' +
+  //       '</div>' +
+  //     '</div>' +
+  //     '<div class="row">' +
+  //       '<div class="col-sm-12">' +
+  //         '<div class="project-name"><i class="fa fa-trophy"></i><br />Activities</div>' +
+  //         '<div class="project-description resume"></div>' +
+  //       '</div>' +
   //     '</div>'
   //   );
+  //   $.each( contentInfo[title].awards, function(i, val) {
+  //     sectionContainer.find($(".project-description")).first().append('<div class="resume-line">' + this + '</div>');
+  //   });
 
-  //   embedpdf();
+  //   $.each( contentInfo[title].activities, function(i, val) {
+  //     sectionContainer.find($(".project-description")).last().append('<div class="resume-line">' + this + '</div>');
+  //   });
   // }
-
-  // function embedPDF(){
-
-  //   var myPDF = new PDFObject({ 
-
-  //     url: 'http://gretahuang.me/resume.pdf'
-
-  //   }).embed('pdf-row'); 
-  //   // Be sure your document contains an element with the ID 'pdf-row' 
-
-  // }
-// }
+}
