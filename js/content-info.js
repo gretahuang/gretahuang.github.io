d183529d8f49940dd4cc7d98aa9cd2cc31d23e94
Greta Huang <gretahuang@berkeley.edu>
2016-08-30 21:46:17 -0700

    Updated resume

--- js/content-info.js
+++ js/content-info.js
@@ -7,7 +7,7 @@ var contentInfo = {
                 "slogan": "Rate your landlord, anonymously.",
                 "description": "LoCality allows UC Berkeley students to rate their landlords anonymously (in progress). Students can sponsor their landlords, if they feel that their landlord deserving. A sponsorship gives the landlord access to advertisement on the UC Berkeley specific leasing and subletting portion of the website.  Skills used include HTML, CSS, JavaScript, SQLAlchemy, Flask.",
                 "link": "https://github.com/gretahuang/calhacks2015",
-                "color": "#d35400"
+                "color": "#ffa800"
             },
             {
                 "title": "Student Progress Tracker",
@@ -39,59 +39,4 @@ var contentInfo = {
             },
         ]
     }
-    // ,
-    // "Resume": {
-    //     "awards": [
-    //         "Technology Student Association NJ Conference – 1st Place in VEX Robotics Tournament",
-    //         "Technology Student Association NJ Conference – 1st Place in VEX Robotics Robot Skills",
-    //         "Monmouth Junior Science Symposium 2014 - Special Recognition for Research on Railguns, Awarded by the Navy",
-    //         "Monmouth Junior Science Symposium 2014 - 2nd Place Research Paper (Awarded by the Armed Forces Communications and Electronics Association)",
-    //         "Columbia University’s Science Honors Program Student",
-    //         "Provisional Patent Recipient",
-    //         "AP Scholar with Distinction",
-    //         "Young Rewired State Festival of Code, NYC – 1st Place, \"Best in Show\"",
-    //         "Young Rewired State Festival of Code, UK – USA Representative, Special Guest Speaker",
-    //         "Merck State Science Day – 3rd Place Team in Chemistry",
-    //         "Merck State Science Day – 34th Place Individual in Chemistry",
-    //         "New Jersey Science League – 1st Place Team in Chemistry II",
-    //         "New Jersey Science League – 30th Place Individual in Chemistry II (Top 10%)",
-    //         "New Jersey Key Club – Distinguished Club Editor",
-    //         "National Merit Scholarship Semifinalist",
-    //         "American Junior Academy of Sciences Delegate (for 2014)",
-    //         "New Jersey Academy of Sciences – 1st Place, Physics / Engineering and Grant-in Aid Recipient",
-    //         "Delaware Valley Science Fair – 3rd Place, Team Projects (Engineering)",
-    //         "Leonardo Da Vinci Society Scholarship – 1st Place in Engineering",
-    //         "Monmouth Junior Science Symposium Poster Fair – 1st Place in Engineering and Physics",
-    //         "Jersey Shore Science Fair – 1st Place in Team Projects (Engineering)",
-    //         "Panasonic Robotics Creative Design Challenge – Finalist and Best Oral Presentation",
-    //         "Technology Student Association NJ Conference – 2nd Place in VEX Robotics",
-    //         "Marlboro High School Varsity Swim Team – Two-time Division Champion",
-    //         "Monmouth County High School Varsity Swimming Championship – 2nd Place Team",
-    //         "Northern Region NJ YMCA Swimming Bronze Championship – 7th Place in Butterfly",
-    //         "Pennsylvania Regional Robotics ROV Competition - 2nd Place",
-    //         "Jersey Shore Science Fair - 3rd Place in Physics",
-    //         "New Jersey Science League - 4th Place Team in Physics I",
-    //         "New Jersey Science League - 40th Place Individual in Physics I",
-    //         "Deloitte Oil Spill Challenge - 20th Place Nationally",
-    //         "NYU-Poly High School Forensics Challenge – Two-time National Finalist",
-    //         "NJ State Swimming Silver/Bronze Division Competition - 11th Place in Backstroke"
-    //     ],
-    //     "activities": [
-    //         "Marlboro High School Varsity Swim Team - Captain",
-    //         "Freehold-Old Bridge YMCA Winter Swim Team",
-    //         "Marlboro Township Summer Swim Team",
-    //         "Key Club – Officer, Editor",
-    //         "Robotics Club – Electronics Team Leader",
-    //         "Competitive NJ Physics C League Team",
-    //         "Competitive NJ Physics I League Team",
-    //         "Competitive NJ Chemistry II League Team",
-    //         "American Computer Science League",
-    //         "Technology Student Association",
-    //         "Math League",
-    //         "Classical / Jazz Band – Alto Saxophone",
-    //         "Volunteering Swim Marathon for American Cancer Society",
-    //         "National Honor Society",
-    //         "Certified Lifeguard"
-    //     ]
-    // }
 }
