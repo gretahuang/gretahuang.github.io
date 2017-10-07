1d155f68c34d76ee33686c8e939e0528d7cea852
Greta Huang <gretahuang@berkeley.edu>
2017-10-06 21:53:00 -0400

    Update resume and replate website.

--- js/content-info.js
+++ js/content-info.js
@@ -6,7 +6,7 @@ var contentInfo = {
                 "image": "replate.png",
                 "slogan": "Everybody Eats!",
                 "description": "Replate is a non-profit organization that matches extra food from businesses or individuals to communities in need. Our platform enables charities and people to recover food donations based on need and location. Ongoing work with a team of Berkeley students through the Cal Blueprint organization.",
-                "link": "https://replate-staging.herokuapp.com/",
+                "link": "https://replate.herokuapp.org/",
                 "color": "#2d8659"
             },
             {
